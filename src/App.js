3ff14a8a85c192ec04cbb74475d24a4798090359
richa <richardmel@outlook.com>
2019-03-20 12:49:57 -0500

    enhanced the app - added routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './rkmTubeLogo.svg';
 import './App.css';
 import Youtube from './Api/Youtube';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -9,9 +10,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+
           <p>
             Welcome to Rkm-Tube
           </p>
+
+          <h2>
+            <Link to="/how-to">How To Use</Link>
+          </h2>
         </header>
 
         <Youtube />
--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HowTo from './routes/HowTo';
+import { BrowserRouter, Route } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+    <BrowserRouter >
+
+      <div>
+
+        <Route path="/" exact component={App}></Route>
+        <Route path="/how-to" component={HowTo}></Route>
+
+      </div>
+    </BrowserRouter>
+
+  , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//  <App />
